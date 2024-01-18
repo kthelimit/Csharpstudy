2a0b6be2275babdbed1915380dda49086cfbeef2
K <73793167+kthelimit@users.noreply.github.com>
2024-01-18 18:26:26 +0900

    Create Cat.cs

---
+++ 240118/UpcastingDowncasting/Cat.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncasting
+{
+    internal class Cat : Animal
+    {
+        public Cat()
+        {
+            Console.WriteLine("Cat 생성자 실행");
+        }
+    }
+}
