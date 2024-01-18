d1aa5057c6dad442ab671559eaef6a4f97a657d7
K <73793167+kthelimit@users.noreply.github.com>
2024-01-18 18:29:12 +0900

    Create StarcraftUnit.cs

---
+++ 240118/Inheritance/StarcraftUnit.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class StarcraftUnit
+    {
+        public string name;
+
+        public StarcraftUnit() {
+            Console.WriteLine("StarcraftUnit 생성자 실행");
+        }
+
+    }
+}
