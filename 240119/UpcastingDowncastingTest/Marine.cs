7ff49dff7ece614ce84dc3cb81f04e981ae4775b
K <73793167+kthelimit@users.noreply.github.com>
2024-01-19 18:05:44 +0900

    Create Marine.cs

---
+++ 240119/UpcastingDowncastingTest/Marine.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpcastingDowncastingTest
+{
+    internal class Marine : TerranUnit
+    {
+
+        public void StimPack()
+        {
+
+            Console.WriteLine("스팀팩을 활성화 합니다.");
+
+        }
+
+
+
+    }
+}
