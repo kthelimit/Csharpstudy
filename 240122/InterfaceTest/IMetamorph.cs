df0c357d789ff60077f45e265b672c0b4b17e8a5
K <73793167+kthelimit@users.noreply.github.com>
2024-01-22 18:11:20 +0900

    Create IMetamorph.cs

---
+++ 240122/InterfaceTest/IMetamorph.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    internal interface IMetamorph
+    {
+        void Morph(string name); // 변형. ex) 드론 => 저글링, 히드라리스크=>러커 
+    }
+}
